ea7b32cc65d74991a9a6068b212da405693dabae
alexisPennel <alexispennel@gmail.com>
2024-05-07 03:39:40 +0200

    feat: add anim

--- app/components/BannerAbout/BannerAbout.jsx
+++ app/components/BannerAbout/BannerAbout.jsx
@@ -1,18 +1,33 @@
 'use client'
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './BannerAbout.module.css';
-import { motion } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import aboutBanner from '../../../public/images/bannerAbout.jpg';
 import Image from 'next/image';
 
 const BannerAbout = () => {
+    const ref = useRef(null);
+
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start end", "end start"]
+    });
+
+    const translateX = useTransform(
+        scrollYProgress,
+        // Map x from these values:
+        [0, 1],
+        // Into these values:
+        ["-10%", "0%"]
+    );
+
     return (
-        <motion.div 
-            initial={{opacity:0.1, y:20}}
-            whileInView={{opacity:1, y:0}}
-            transition={{delay:0.3, ease:'easeInOut'}}
-            onContextMenu={(event) => event.preventDefault()}>
-            <Image src={aboutBanner} width={1920} height={2880} alt="Photo de Myogi, photographe animalier et automobile de Rouen" className={styles.about__banner}  tabIndex={0} noindex="true" draggable="false"/>
+        <motion.div
+            onContextMenu={(event) => event.preventDefault()}
+            ref={ref}>
+            <motion.div style={{ translateX: translateX }}>
+                <Image src={aboutBanner} width={1920} height={2880} alt="Photo de Myogi, photographe animalier et automobile de Rouen" className={styles.about__banner} tabIndex={0} noindex="true" draggable="false" />
+            </motion.div>
         </motion.div>
     );
 };
